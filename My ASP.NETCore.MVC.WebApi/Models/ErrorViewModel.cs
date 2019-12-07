635cfc49e38e1cbdc1017a32a9551ff5777a0d76
nicoleputois <nicoleputois@hotmail.com>
2019-12-07 22:59:20 +0100

    Ajoutez des fichiers projet.

---
+++ My ASP.NETCore.MVC.WebApi/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace My_ASP.NETCore.MVC.WebApi.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
